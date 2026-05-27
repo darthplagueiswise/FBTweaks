3e209b5cecb9b32ee498aa28df5da76a1c2b32a1
euoradan <zelihmannradan@gmail.com>
2026-05-27 19:25:11 -0300

    Fix C linkage for log API

--- src/Runtime/FBGRLog.h
+++ src/Runtime/FBGRLog.h
@@ -1,5 +1,18 @@
 #pragma once
 #import <Foundation/Foundation.h>
-void    FBGRLogAppend(NSString *msg);
+
+NS_ASSUME_NONNULL_BEGIN
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void FBGRLogAppend(NSString *msg);
 NSString *FBGRLogSnapshot(void);
-void    FBGRLogClear(void);
+void FBGRLogClear(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+NS_ASSUME_NONNULL_END
