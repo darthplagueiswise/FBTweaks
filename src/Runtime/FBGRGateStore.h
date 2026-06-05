7423abb14fea461e2e73325fdbaa2912934beac8
euoradan <zelihmannradan@gmail.com>
2026-06-04 23:22:58 -0300

    Fix C linkage for GateStore exports

--- src/Runtime/FBGRGateStore.h
+++ src/Runtime/FBGRGateStore.h
@@ -1,5 +1,10 @@
 #pragma once
 #import <Foundation/Foundation.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 BOOL FBGRGateIsSet(uint64_t slotId);
 BOOL FBGRGateGet(uint64_t slotId);
 void FBGRGateSet(uint64_t slotId, BOOL value);
@@ -7,3 +12,7 @@ void FBGRGateClear(uint64_t slotId);
 void FBGRGateClearAll(void);
 void FBGRGateWarmCacheFromPrefs(void);
 NSArray<NSNumber *> *FBGRGateAllOverrideSlotIds(void);
+
+#ifdef __cplusplus
+}
+#endif
