2dfff3ba9f1b97392e46441f9f104b04429df0b5
euoradan <zelihmannradan@gmail.com>
2026-07-20 02:48:06 -0300

    v4.1: declare native MobileConfig UI warmup hooks

---
+++ src/Runtime/FBTMobileConfigDebugUIHooks.h
@@ -0,0 +1,7 @@
+#ifndef FBT_MOBILECONFIG_DEBUG_UI_HOOKS_H
+#define FBT_MOBILECONFIG_DEBUG_UI_HOOKS_H
+
+void FBTInstallMobileConfigDebugUIBootstrap(void);
+void FBTInstallMobileConfigDebugUIHooks(void);
+
+#endif /* FBT_MOBILECONFIG_DEBUG_UI_HOOKS_H */
