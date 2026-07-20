f429f4ac686923dd4fe981e7049ccf3b1aca5af5
euoradan <zelihmannradan@gmail.com>
2026-07-20 02:33:37 -0300

    v4.0: add React Native internal hook interface

---
+++ src/Features/ReactNative/FBTReactNativeInternal.h
@@ -0,0 +1,7 @@
+#ifndef FBT_REACT_NATIVE_INTERNAL_H
+#define FBT_REACT_NATIVE_INTERNAL_H
+
+void FBTInitReactNativeInternalGroup(void);
+void FBTInstallReactNativeAndBuildImportHooks(void);
+
+#endif /* FBT_REACT_NATIVE_INTERNAL_H */
