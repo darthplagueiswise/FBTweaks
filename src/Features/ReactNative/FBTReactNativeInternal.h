a227d057dde6ee7d01511e7a69ba536a22d70e97
euoradan <zelihmannradan@gmail.com>
2026-07-20 02:57:00 -0300

    feat(rn): expose native RN Internal Settings opener

--- src/Features/ReactNative/FBTReactNativeInternal.h
+++ src/Features/ReactNative/FBTReactNativeInternal.h
@@ -1,7 +1,10 @@
 #ifndef FBT_REACT_NATIVE_INTERNAL_H
 #define FBT_REACT_NATIVE_INTERNAL_H
 
+#import <Foundation/Foundation.h>
+
 void FBTInitReactNativeInternalGroup(void);
 void FBTInstallReactNativeAndBuildImportHooks(void);
+void FBTOpenReactNativeInternalSettings(void);
 
 #endif /* FBT_REACT_NATIVE_INTERNAL_H */
