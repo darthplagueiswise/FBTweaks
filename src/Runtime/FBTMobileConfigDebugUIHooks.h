cf55768e7c3d0f3747d7ccc772dcb4190110121d
euoradan <zelihmannradan@gmail.com>
2026-07-20 02:52:37 -0300

    v4.1: make MobileConfig debug UI hooks ObjC++ safe

--- src/Runtime/FBTMobileConfigDebugUIHooks.h
+++ src/Runtime/FBTMobileConfigDebugUIHooks.h
@@ -1,7 +1,15 @@
 #ifndef FBT_MOBILECONFIG_DEBUG_UI_HOOKS_H
 #define FBT_MOBILECONFIG_DEBUG_UI_HOOKS_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void FBTInstallMobileConfigDebugUIBootstrap(void);
 void FBTInstallMobileConfigDebugUIHooks(void);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* FBT_MOBILECONFIG_DEBUG_UI_HOOKS_H */
